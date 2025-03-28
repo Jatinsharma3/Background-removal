332548f9dedeac14f694e9b2b802c85809d5f603
Jatinsharma3 <sunilsharma97160@gmail.com>
2025-03-29 02:10:06 +0530

    add server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+// App config
+const PORT = process.env.port || 4000
+const app = express()
+await connectDB()
+
+// Initialize Middlewares
+app.use(express.json())
+app.use(cors())
+
+// api routes
+app.get('/',(req,res)=> res.send("API Working"))
+
+app.listen(PORT, ()=> console.log("Server Running on port" + PORT))
+
