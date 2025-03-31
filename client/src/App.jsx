bce9eca383fd1406b43ae29efddef73b57b54475
Jatinsharma3 <sunilsharma97160@gmail.com>
2025-04-01 02:43:27 +0530

    add credits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Result from './pages/result'
 import BuyCredit from './pages/buyCredit'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div className='min-h-screen bg-slate-50'>
+      <ToastContainer position='bottom-right' /> 
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
---
+++ server/controllers/ImageController.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import fs from 'fs'
+import FormData from 'form-data'
+import userModel from '../models/userModel.js'
+
+// function to remove the background
+const removeBgImage = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
